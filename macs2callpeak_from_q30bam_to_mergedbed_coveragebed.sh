aaeb89ca4827d831ab93e7457006969a34a272ed
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2021-09-10 13:10:10 -0400

    Update macs2callpeak_from_q30bam_to_mergedbed_coveragebed.sh

--- macs2callpeak_from_q30bam_to_mergedbed_coveragebed.sh
+++ macs2callpeak_from_q30bam_to_mergedbed_coveragebed.sh
@@ -6,11 +6,12 @@ path=~/Desktop/Sung_work/sam
 
 for j in 1 2 3; do macs2 callpeak -t $path/${input}${j}.36.q30.sorted2.rmd.bam -f BAMPE -g mm -n ${input}${j} -B -q 0.05 --SPMR --outdir $path/macs2; done
 
-##### generate merged.bed as the input for bedtools coverage
+##### generate merged.bed as the first input for bedtools coverage
 cd $path/macs2/
-cat *Peak | sort -k1,1 -k2,2n | bedtools merge -i stdin | awk 'BEGIN{OFS="\t"} $0=$0"\t"NR' > merged.bed
+cat *Peak | sort -k1,1 -k2,2n | bedtools merge -i stdin | awk 'BEGIN{OFS="\t"} $0=$0"\t"NR' > ${input}.merged.bed
 
-##### generate each paired end bed files from bam files 
+##### generate each paired end bed files from bam files
+##### this is also the second input file for bedtools coverage
 for k in 1 2 3; do bedtools bamtobed -i $path/${input}${k}.36.q30.sorted2.rmd.bam > $path/${input}${k}.pe.bed; done
 
 ##### bedtools coverage: how much of the genome does my data cover?

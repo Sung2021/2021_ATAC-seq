c8535856e68e72ebd79949a2667e6a7cc3b398c5
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-07-06 15:26:51 -0400

    Update macs2.for.bw.sh

--- macs2/macs2.for.bw.sh
+++ macs2/macs2.for.bw.sh
@@ -17,3 +17,14 @@ path=.
 mkdir $path/macs2.out
 for i in *.bam;do mkdir $path/macs2.out/${i}
 macs2 callpeak -t $path/${i} -f BAMPE -g mm -n ${i} -B -q 0.05 --outdir $path/macs2.out/${i} ;done
+
+#!/bin/bash
+path=.
+for i in WT1 WT2 WT3 KO1 KO2; do bedGraphToBigWig $path/Icos_${i}.final.bam.sorted.bam/Icos_${i}.final.bam.sorted.bam_treat_pileup.bdg \
+~/Desktop/ref/mm10/mm10.chrom.sizes.txt ${i}.pileup.bw ; done 
+
+## in R
+library(rtracklayer)
+bw <- import('~/Desktop/Sung_work/sam/ATAC_seq/Tle3/macs2_conditional/test.bw')
+bw[1:3,]
+bw$score/sum(coverage)*10^6

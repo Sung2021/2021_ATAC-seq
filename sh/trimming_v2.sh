bacdd5d8764aa9d93a9821b805cb56e9cdbeb2ba
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-07-22 11:59:10 -0400

    Create trimming_v2.sh

---
+++ sh/trimming_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path=.
+
+### Trimming to keep 36bp
+## run
+crop=36
+day=d05
+
+for input in wt1 wt2 wt3 ko1 ko2 ko3; do crop=36
+java -jar ~/Desktop/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 10 \
+${day}.${input}.R1.fastq.gz ${day}.${input}.R2.fastq.gz \
+$path/trimmed/${day}.${input}.${crop}.R1.paired.fastq.gz $path/trimmed/${day}.${input}.${crop}.R1.unpaired.fastq.gz \
+$path/trimmed/${day}.${input}.${crop}.R2.paired.fastq.gz $path/trimmed/${day}.${input}.${crop}.R2.unpaired.fastq.gz \
+CROP:${crop}; done
+

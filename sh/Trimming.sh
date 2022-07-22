e039d074aa5c4a210090cb654cd0348707b7c0e8
Sung2021 <90470359+Sung2021@users.noreply.github.com>
2022-07-22 12:17:08 -0400

    Update Trimming.sh

--- sh/Trimming.sh
+++ sh/Trimming.sh
@@ -6,7 +6,7 @@ path=.
 ## run
 crop=36
 
-for input in Icos_WT2 Icos_WT3 Icos_KO1 Icos_KO2 Icos_KO3; do crop=36
+for input in Icos_WT2 Icos_WT3 Icos_KO1 Icos_KO2 Icos_KO3; do 
 java -jar ~/Desktop/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 10 \
 ${input}_R1_001.fastq.gz ${input}_R2_001.fastq.gz \
 $path/${input}.${crop}.R1.paired.fastq.gz $path/${input}.${crop}.R1.unpaired.fastq.gz \
